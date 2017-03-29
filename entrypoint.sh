2ca26ee7ec7c9231f434b45e1e7f3c35690b57d0
Glenn Slaven <glenn@section.io>
2017-03-29 13:05:34 +1100

    Run the task in its own namespace, allowing for job to not exist at startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,38 @@
 #! /bin/bash
+test -z "${DEBUG}" || set -o xtrace
+
+function active_job_exists() {
+
+    job=$(kubectl get job sitespeedio --namespace "${SELF_NAMESPACE}" --output json)
+    [[ $? -eq 1 ]] && return 1 # If error job does not exist
+
+    active=$(echo $job | jq '.status.active')
+    [[ "${active}" == "null" ]] && return 1 # If `.status.active` is null then there is no active job
+
+    return 0
+}
 
 while true; do
-    response=$(redis-cli -h redis.section-sitespeed RPOPLPUSH queue queue)
+
+    # Poll for the job to finish before moving onto the next job
+    while active_job_exists; do
+        sleep 5
+    done
+
+    # Take a message from the queue
+    response=$(redis-cli -h "${REDIS_HOST}" RPOPLPUSH "${QUEUE_NAME}" "${QUEUE_NAME}")
     message=$(echo "${response}" | tail -1)
 
-    namespace=$(echo "${message}" | jq '.namespace' --raw-output)
-    image=$(echo "${message}" | jq '.image' --raw-output)
-    argarray=($(echo "${message}" | jq '.args[]' --raw-output))
-    args=$(printf "        - %s\n" "${argarray[@]}")
-
-    # Check if this namespace has an active job already
-    active=$(kubectl get job sitespeedio --namespace "${namespace}" --output json | jq '.status.active')
-    if [[ "${active}" == 'null' ]]; then
+    if [[ ! -z "${message}" ]]; then
+        client_namespace=$(echo "${message}" | jq '.namespace' --raw-output)
+        image=$(echo "${message}" | jq '.image' --raw-output)
+        argarray=($(echo "${message}" | jq '.args[]' --raw-output))
+        args=$(printf "        - %s\n" "${argarray[@]}")
 
         # Delete the old job
-        kubectl delete job sitespeedio --namespace "${namespace}" || echo "Job doesn't exist"
+        kubectl delete job sitespeedio --namespace "${SELF_NAMESPACE}" || echo "Job doesn't exist"
 
         # Create a new job
-        namespace="${namespace}" args="${args}" image="${image}" envsubst < /sitedeployment.yml.template | kubectl apply -f -
-        active=1
-
-        # Poll for the job to finish before moving onto the next job
-        while [[ "${active}" == 1 ]]; do
-            sleep 5
-            active=$(kubectl get job sitespeedio --namespace "${namespace}" --output json | jq '.status.active')
-        done
+        client_namespace="${client_namespace}" args="${args}" image="${image}" envsubst < /sitedeployment.yml.template | kubectl apply --namespace "${SELF_NAMESPACE}" -f -
     fi
 done
