c974d0bd133880f9837af5cd51602238772710e1
Glenn Slaven <glenn@section.io>
2017-04-05 15:03:09 +1000

    Allow the graphite host suffix to be blank so graphite can be in the same namespace

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,14 +25,20 @@ while true; do
 
     if [[ ! -z "${message}" ]]; then
         client_namespace=$(echo "${message}" | jq '.namespace' --raw-output)
+        graphite_namespace=$(echo "${message}" | jq '.graphite_namespace' --raw-output)
         image=$(echo "${message}" | jq '.image' --raw-output)
         argarray=($(echo "${message}" | jq '.args[]' --raw-output))
         args=$(printf "        - %s\n" "${argarray[@]}")
+
+        graphite_host_suffix=''
+        if [[ "${client_namespace}" != 'null' ]]; then
+            graphite_host_suffix=".${client_namespace}"
+        fi
 
         # Delete the old job
         kubectl delete job sitespeedio --namespace "${SELF_NAMESPACE}" || echo "Job doesn't exist"
 
         # Create a new job
-        client_namespace="${client_namespace}" args="${args}" image="${image}" envsubst < /sitedeployment.yml.template | kubectl apply --namespace "${SELF_NAMESPACE}" -f -
+        graphite_host_suffix="${graphite_host_suffix}" graphite_namespace="${graphite_namespace}" args="${args}" image="${image}" envsubst < /sitedeployment.yml.template | kubectl apply --namespace "${SELF_NAMESPACE}" -f -
     fi
 done
